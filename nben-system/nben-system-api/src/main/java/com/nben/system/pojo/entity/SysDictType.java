40412e981209cd4be63b3e98b2c435dc10175b1d
fourcows <fourcows@163.com>
2021-12-16 22:38:32 +0800

    refactor: 使用lombok重构system模块中的所有pojo类

--- nben-system/nben-system-api/src/main/java/com/nben/system/pojo/entity/SysDictType.java
+++ nben-system/nben-system-api/src/main/java/com/nben/system/pojo/entity/SysDictType.java
@@ -1,94 +1,36 @@
 package com.nben.system.pojo.entity;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import com.nben.common.core.annotation.Excel;
 import com.nben.common.core.annotation.Excel.ColumnType;
 import com.nben.common.core.web.domain.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-/**
- * 字典类型表 sys_dict_type
- *
- * @author ruoyi
- */
-public class SysDictType extends BaseEntity
-{
-    private static final long serialVersionUID = 1L;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-    /** 字典主键 */
-    @Excel(name = "字典主键", cellType = ColumnType.NUMERIC)
-    private Long dictId;
+/** 字典类型表 sys_dict_type */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SysDictType extends BaseEntity {
 
-    /** 字典名称 */
-    @Excel(name = "字典名称")
-    private String dictName;
+  /** 字典主键 */
+  @Excel(name = "字典主键", cellType = ColumnType.NUMERIC)
+  private Long dictId;
 
-    /** 字典类型 */
-    @Excel(name = "字典类型")
-    private String dictType;
+  /** 字典名称 */
+  @Excel(name = "字典名称")
+  @NotBlank(message = "字典名称不能为空")
+  @Size(min = 0, max = 100, message = "字典类型名称长度不能超过100个字符")
+  private String dictName;
 
-    /** 状态（0正常 1停用） */
-    @Excel(name = "状态", readConverterExp = "0=正常,1=停用")
-    private String status;
+  /** 字典类型 */
+  @Excel(name = "字典类型")
+  @NotBlank(message = "字典类型不能为空")
+  @Size(min = 0, max = 100, message = "字典类型类型长度不能超过100个字符")
+  private String dictType;
 
-    public Long getDictId()
-    {
-        return dictId;
-    }
-
-    public void setDictId(Long dictId)
-    {
-        this.dictId = dictId;
-    }
-
-    @NotBlank(message = "字典名称不能为空")
-    @Size(min = 0, max = 100, message = "字典类型名称长度不能超过100个字符")
-    public String getDictName()
-    {
-        return dictName;
-    }
-
-    public void setDictName(String dictName)
-    {
-        this.dictName = dictName;
-    }
-
-    @NotBlank(message = "字典类型不能为空")
-    @Size(min = 0, max = 100, message = "字典类型类型长度不能超过100个字符")
-    public String getDictType()
-    {
-        return dictType;
-    }
-
-    public void setDictType(String dictType)
-    {
-        this.dictType = dictType;
-    }
-
-    public String getStatus()
-    {
-        return status;
-    }
-
-    public void setStatus(String status)
-    {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("dictId", getDictId())
-            .append("dictName", getDictName())
-            .append("dictType", getDictType())
-            .append("status", getStatus())
-            .append("createBy", getCreateBy())
-            .append("createTime", getCreateTime())
-            .append("updateBy", getUpdateBy())
-            .append("updateTime", getUpdateTime())
-            .append("remark", getRemark())
-            .toString();
-    }
+  /** 状态（0正常 1停用） */
+  @Excel(name = "状态", readConverterExp = "0=正常,1=停用")
+  private String status;
 }
